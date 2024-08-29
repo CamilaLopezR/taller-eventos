73206d4708d861846be6dfbaf4cbf48ca2385e1d
CamilaLopezR <camila.lopez.russell@gmail.com>
2024-08-29 12:01:11 -0300

    si

--- eventos.js
+++ eventos.js
@@ -1,6 +1,8 @@
+
 document.getElementById("miDiv").addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
+
 document.getElementById("miBoton").addEventListener("click", function(event) {
     alert('Hola!');
     event.stopPropagation(); 
